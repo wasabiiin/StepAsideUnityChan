09ce29e0b335663308eb07121840eb5f9c0b28bf
norimi.akai <noriiim1@gmail.com>
2017-12-14 22:51:19 +0900

    再提出

---
+++ Assets/Destroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    private bool isVisible;
+
+    // 可視状態か
+    public bool IsVisible
+    {
+        get { return isVisible; }
+    }
+
+    // 可視状態になった時に呼ばれる
+    void OnBecameVisible()
+    {
+        isVisible = true;
+    }
+
+    // 不可視状態になった時に呼ばれる
+    void OnBecameInvisible()
+    {
+        isVisible = false;
+    }
+
+}
